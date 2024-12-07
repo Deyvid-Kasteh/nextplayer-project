bc5d526779a78d8b469f2a5fab99ee2cc845e5aa
Deyvid Kasteh <kasteh21@gmail.com>
2024-12-07 15:32:32 -0300

    new next project

--- .history/src/app/page_20241207153130.tsx
+++ .history/src/app/page_20241207153130.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs KASTEH
+            Read our docs kasteh
           </a>
         </div>
       </main>
